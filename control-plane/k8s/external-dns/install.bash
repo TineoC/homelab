f29a090a46aeb4de638c0c23139574a5722200f3
Christopher Tineo <christophertineo02@gmail.com>
2025-03-18 12:00:50 -0400

    install argocd

--- control-plane/k8s/external-dns/install.bash
+++
@@ -1,9 +0,0 @@
-helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
-helm upgrade --install external-dns bitnami/external-dns \
-  --namespace kube-system \
-  --create-namespace \
-  --set provider=cloudflare \
-  --set domainFilters[0]=tineochristopher.com \
-  --set cloudflare.apiToken=76Xw6_8JnKYCQ-V7Tggi41Ht9mwLzXVRtrbrlKDO \
-  --set policy=sync \
-  --set txtOwnerId=external-dns
